b04ebe1ce750d26aad386c8364a647c9c1c2ac4c
Kagami <shimura.kyohei7617@gmail.com>
2024-04-27 18:07:38 +0900

    auto: auto command push

--- nginx_files/main.js
+++ nginx_files/main.js
@@ -1,1 +1,9 @@
-console.log('Hello JavaScript')
+async function main() {
+  console.log('Hello JavaScript');
+  const response = await fetch('/api/hello');
+  console.log(response);
+  const responseBody = await response.json();
+  console.log(responseBody);
+}
+
+main();
