86bcaf4874df74b1fc40e525b09aaf98e4ee0cdb
Kagami <shimura.kyohei7617@gmail.com>
2024-04-27 21:23:38 +0900

    auto: auto command push

--- myapp/myapp.rb
+++ myapp/myapp.rb
@@ -1,12 +1,29 @@
- require 'sinatra'
-    get '/' do
-      'Hello World!'
-    end
-    get '/test' do
-      'Hello Test!'
-    end
-    get '/api/hello' do
-      {
-          message: 'Hello World!'
-      }.to_json
-    end
+require 'sinatra'
+ 
+tasks = [
+  {
+    title: "フロントエンドの実装",
+    createdAt: Time.now
+  },
+     {
+    title: "バックエンドの実装",
+    createdAt: Time.now
+  }
+  ]
+
+   get '/' do
+     'Hello World!'
+   end
+   get '/test' do
+     'Hello Test!'
+   end
+   get '/api/hello' do
+     {
+         message: 'Hello World!'
+     }.to_json
+   end
+   get '/api/tasks' do
+       {
+           tasks: tasks
+       }.to_json
+   end
